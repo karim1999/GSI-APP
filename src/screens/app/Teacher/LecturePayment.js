f4073e3b427cb506cfa2a3f9bde2d6261ad25ac9
Karim Mahmoud <karim.elbadry2@gmail.com>
2018-12-31 02:04:36 +0200

    Everything

---
+++ src/screens/app/Teacher/LecturePayment.js
@@ -0,0 +1,182 @@
+import React, { Component } from 'react';
+import { StyleSheet, Text, View, Image, FlatList } from 'react-native';
+import { Icon, H3, Form, Item, Button, Label, ListItem, Left, Body, Right, Thumbnail, Card, CardItem, Input} from 'native-base';
+import Color from '../../../constants/colors';
+import AppTemplate from "../appTemplate";
+import _ from 'lodash'
+
+export default class LecturePayment extends Component {
+    constructor(props){
+        super(props);
+        this.state = {
+            title: this.props.navigation.state.params.title,
+            lecture: this.props.navigation.state.params.joint_users,
+            searchLectures: this.props.navigation.state.params.joint_users,
+            student: "",
+            random:['#d93232', '#636c8f', '#6c856c', '#fbaf5d'],            
+        }
+    }
+
+    // componentDidMount(){
+    //     this.setState({
+    //         isLoading: true
+    //     })
+    //     return AsyncStorage.getItem('token').then(userToken => {
+    //         return axios.get(Server.url + 'api/getusers?token='+userToken)
+    //         .then(response => {
+    //             this.setState({
+    //                 isLoading: false,
+    //                 showLectAndUser: response.data
+    //             })
+    //         }).catch(error => {
+    //             Toast.show({
+    //                 text: 'Error reachig server',
+    //                 buttonText: "Ok",
+    //                 type: "danger"
+    //             })
+    //         })
+    //     })
+    // alert(JSON.stringify(this.state.lecture))
+    
+    // }
+
+    async Data(){
+        let data = [];
+        data = this.state.lecture;
+
+        if(this.state.student !== ""){
+            data = await _.filter(data, lecture => lecture.name.toLowerCase().indexOf(this.state.student.toLowerCase()) > -1);
+        }
+      
+      this.setState({
+          searchLectures: data
+      })
+      
+    }
+    
+    render() {
+        return (
+            <AppTemplate back navigation={this.props.navigation} title = {this.state.title}>  
+                <View style={styles.content}>
+
+                <View style={styles.Box}>
+                    <Item style={{height: 70, padding: 15, paddingBottom:0, backgroundColor: '#fff', borderColor: 'transparent' }}>
+                        <Icon type="FontAwesome" name='user' />
+                        <Label style={styles.font}>Student Name </Label>
+                        <Input
+                                onChangeText={(student) => this.setState({student})}
+                                placeholder="Name..."
+                                placeholderTextColor="#ccc5c5"
+                        />
+                    </Item>
+
+                    <Button style={styles.button} onPress={ () => this.Data() }>
+                        <Text style={styles.buttonTxt}>Search</Text>
+                    </Button>
+
+                </View>
+
+                <FlatList
+                    ListEmptyComponent={
+                        <Text style={{ fontSize: 20, fontFamily: "Roboto", padding:5, flex: 1, textAlign: "center"}}>No one joint ur lectures</Text>
+                    }
+                    data={this.state.searchLectures}
+                    renderItem={({item}) => (
+
+                    <View style={styles.Box1}>  
+                        <Card style={{borderWidth: 0}} transparent={true}>
+                        
+                            <CardItem style={{}}>
+                                <Left>
+                                <Thumbnail source={require('../../../images/Background.png')} />
+                                <Text style={{paddingLeft: 10, fontSize: 19, fontFamily: "Roboto",}}>{item.name}</Text>
+                                </Left>
+                                <Right style={styles.allStarsComment}>
+                                    <View style={{ width: 10, height: 80, backgroundColor: this.state.random[item.id % 4], 
+                                    borderRadius: 5}}></View>
+                                </Right> 
+                            </CardItem>
+                            <ListItem style={styles.list}>
+                                <Body>
+                                <H3 style={styles.font}>Paid</H3>
+                                </Body>
+                                <Right>
+                                    <Label style={styles.font}>{item.pivot.amount}$</Label>
+                                </Right>
+                            </ListItem>
+                            <ListItem style={styles.list}>
+                                <Body>
+                                <H3 style={styles.font}>Need to be paid</H3>
+                                </Body>
+                                 <Right>
+                                    {
+                                        ((this.state.lecture.price - item.pivot.amount) == 0 ) ? 
+                                        (
+                                            <Label style={styles.font}>None</Label>
+                                        ):
+                                        (
+                                            <Label style={styles.font}>{this.state.lecture.price - item.pivot.amount}$</Label>
+                                        )
+                                    }
+                                </Right>
+                            </ListItem>
+                        </Card>
+                    </View>
+                )}
+                keyExtractor = { (item, index) => index.toString() }
+                />
+                </View>
+
+            </AppTemplate>
+        );
+    }
+}
+
+const styles = StyleSheet.create({
+    content:{
+        backgroundColor: Color.background,
+        padding:7,
+    },
+    Box: {
+        height: 150, 
+        backgroundColor: '#fff',
+        borderRadius: 5,
+        marginBottom: 30
+    },
+    Box1: {
+        backgroundColor: '#fff',
+        borderRadius: 5,
+        padding: 5, 
+        paddingTop: 0,
+        marginBottom: 30
+    },
+    firstBox:{
+        width: 10, 
+        height: 80, 
+        backgroundColor: Color.fourthColor, 
+        borderRadius: 5
+    },
+    button:{
+        backgroundColor: '#fef5e5',
+        paddingTop: 0,
+        paddingBottom: 0,
+        padding: 30,
+        marginTop: 20,
+        justifyContent: 'center',
+        alignSelf: 'center',
+    },
+    buttonTxt:{
+        color: '#000',
+        fontSize: 20,
+    },
+    font:{
+        fontFamily: "Roboto"
+    },
+    list:{
+        paddingRight: 60,  
+        backgroundColor: '#fff',
+        borderColor: 'transparent'
+    }
+   
+
+});
---
+++ src/screens/auth/SignIn/ForgetPassword.js
@@ -0,0 +1,117 @@
+import React, { Component } from 'react';
+import { StyleSheet, Text, View, Image, TouchableOpacity,ImageBackground, ActivityIndicator } from 'react-native';
+import {Form, Item, Input, Button, Toast} from 'native-base';
+import Colors from "../../../constants/colors";
+import Server from "../../../constants/config"
+import axios from "axios";
+export default class ForgetPassword extends Component {
+    constructor(props){
+        super(props);
+        this.state = {
+           isResetPassword: false,
+           email: "",
+        }
+    }
+    onResetPasswordPressed(){
+        if(this.state.email == ""){
+            
+            Toast.show({
+                text: 'email cannot be empty.',
+                type: "danger",
+                buttonText: 'Okay'
+            });
+        
+        }else{
+             this.setState({
+                isResetPassword:true
+            });
+
+        return axios.post(Server.url+'api/auth/password/reset',{
+
+            email: this.state.email
+            
+        }).then(response => {
+            Toast.show({
+                text: 'A reset email has been sent! Please check your email.',
+                type: 'success',
+                buttonText: 'Okay'
+            });
+            this.setState({
+                isResetPassword: false
+            });
+        }).catch(error => {
+            let text= "No Internet Connection.";
+            if(error.response.status == 401 && error.response.data.error &&  error.response.data.error.email){
+                if(error.response.data.error.email){
+                    text= error.response.data.error.email[0];
+                }
+            }
+            Toast.show({
+                text,
+                type: 'danger',
+                buttonText: 'Okay'
+            });
+            this.setState({
+                isResetPassword: false
+            });
+        });
+        }
+    }
+
+    render() {
+        return (
+            <Form>
+                <ImageBackground style={{width: '100%', height: '100%'}} source={require('../../../images/background.jpg')}>
+                    <Image 
+                    source={require('../../../images/LightenedLogo.png')}
+                     style={{height: 300, width: 300,alignSelf: 'center', }}
+                    />
+                    <Item rounded style={styles.input}>
+                        <Input style={styles.inputText} placeholder="Email" placeholderTextColor="#fff"
+                                onChangeText={(val) => this.setState({email: val})}/>
+                    </Item>
+                    <Button info style={styles.button} onPress={()=>this.onResetPasswordPressed()}>
+                    <Text style={styles.buttonText}> Send Password Reset Link </Text>
+                    {this.state.isResetPassword && (
+                        <ActivityIndicator style={{}} size="small" color="#000000" />
+                    )}
+                    </Button>
+                    <TouchableOpacity style={{alignItems: 'center',}} onPress={()=> this.props.navigation.navigate('SignIn')}>
+                        <Text style={styles.loginButton}>Back to login?</Text>
+                    </TouchableOpacity>
+                </ImageBackground>
+            </Form>
+        );
+    }
+}
+
+const styles = StyleSheet.create({
+    input:{
+        width: 300,
+        marginBottom: 10,
+        padding: 10,
+        height: 40,
+        alignSelf: 'center',
+    },
+    inputText:{
+        color: '#fff',
+        fontSize: 16
+    },
+    button:{
+        alignSelf: 'center',
+        borderRadius: 25,
+        paddingLeft: 40,
+        paddingRight: 40
+    },
+    buttonText:{
+        fontSize: 16,
+        fontWeight: '500',
+        color: '#fff',
+        textAlign: 'center',
+    },
+    loginButton:{
+        color: '#d9cdb7',
+        fontSize: 16,
+        fontFamily: "Roboto",
+    }
+});
